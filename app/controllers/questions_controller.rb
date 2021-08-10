84b4f34e4413cb60eafbfd6c54609fe9167af702
emily doswell <emilydoswell@yahoo.co.uk>
2021-08-10 12:10:41 +0200

    answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    if params[:answer] == "I am going to work"
+      puts "Great!"
+    elsif params[:answer][-1] == "?"
+      puts "Silly question, get dressed and go to work!"
+    else
+      puts "I don't care, get dressed and go to work!"
+    end
   end
 end
